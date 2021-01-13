3f22872ff0c52e125750122219dc3f35ce6ff4ad
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2021-01-12 21:27:31 -0600

    Start of compente app

---
+++ componentes/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componenetes App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
