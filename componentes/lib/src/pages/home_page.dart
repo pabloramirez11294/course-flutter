642f4325d95a4abccac30016cda1ae2d4ea75ac6
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2021-01-21 00:39:13 -0600

    upload json and create method getIcon

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icon_string_util.dart';
 
 class HomePage extends StatelessWidget {
   //const HomePage({Key key}) : super(key: key);
@@ -14,25 +16,37 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    // print(menuProvider.opciones);
+    // menuProvider.cargarData().then((opciones) {
+    //   print('_lista');
+    //   print(opciones);
+    // });
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Opcion1'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Opcion2'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Opcion3'),
-      ),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {},
+      );
+      opciones..add(widTemp)..add(Divider());
+    });
+    return opciones;
   }
 }
